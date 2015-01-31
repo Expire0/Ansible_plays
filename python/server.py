0db3d94a76032ba31058b77a43fd2f78dc380d09
airtime166 <airtime166@yahoo.com>
2015-01-31 13:03:24 -0500

    Create server.py

---
+++ python/server.py
@@ -0,0 +1,18 @@
+geektrainer
+or https://docs.python.org/2/library/simplehttpserver.html
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+@app.route('/')
+def hello():
+    return "hey"
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
