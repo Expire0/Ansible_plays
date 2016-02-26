5d256e64674356df3345456976f688030fdc03ad
airtime166 <airtime166@yahoo.com>
2016-02-26 00:53:14 -0500

    Update server-flask.py

--- python/server-flask.py
+++ python/server-flask.py
@@ -1,5 +1,7 @@
 #sample code to run a server using Flask
 # Make the WSGI interface available at the top level so wfastcgi can get it.
+from flask import Flask
+app = Flask(__name__)
 wsgi_app = app.wsgi_app
 
 @app.route('/')
