a2e44327a960dc9828b707493f8be07cea679640
Mas <airtime166@yahoo.com>
2016-08-26 04:13:10 -0400

    Update v6compress.py

--- python/v6compress.py
+++ python/v6compress.py
@@ -11,3 +11,31 @@ with open('<file>') as mas:
     for i in v6:
         hey = ipaddress.ip_address(i)
         print(hey.compressed)
+
+
+####cleaner way to do it with user defining the file 
+
+#!/usr/bin/env python3
+
+#compress v6 expanded addresses in bulk.  pulls from a local file and based on python3
+
+import ipaddress
+import sys
+menu= input("Enter 1 for v6 compression:  ")
+menu1 = int(menu)
+
+if menu1 == 1:
+    getme = input("filename: ")
+
+    def v6compress(m):
+        i=m
+        with open(i) as mas:
+            v6 = mas.read().splitlines()
+            for i in v6:
+                hey = ipaddress.ip_address(i)
+                print(hey.compressed)
+
+    v6compress(getme)
+
+else:
+    print ("nothing to do")
