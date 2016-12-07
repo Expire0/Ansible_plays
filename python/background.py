aa66af10659fa8d02396fa6fca23bf54ab5508c4
Mas <airtime166@yahoo.com>
2016-12-07 04:09:46 -0500

    Update background.py

--- python/background.py
+++ python/background.py
@@ -47,14 +47,6 @@ python
 
 ### run a backgroup process and send the stout to a file.   Use the file for a status update and delete 
 
-
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 30 00:07:07 2016
-
-@author: mas
-"""
-
 #!/usr/bin/env python3
 
 import subprocess, os , sys
@@ -70,7 +62,6 @@ code= subprocess.call(["./run.sh & "],shell=True)
 
 
 ## check if process is running
-if 
-code= subprocess.call(["tail -f check "],shell=True)
+#code= subprocess.call(["tail -f check "],shell=True)
 
 print(mas)
