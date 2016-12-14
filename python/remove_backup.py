1185a3014b65d28bc07b9d049b550d7900541c0e
alexlpratt <alexlpratt@me.com>
2016-12-13 20:02:55 -0500

    I made 'check' into an importable function

--- python/remove_backup.py
+++ python/remove_backup.py
@@ -10,25 +10,30 @@ from datetime import datetime, timedelta
 from os import listdir
 from os import remove
 
-check = listdir("/var/backup/ldap/")
+
 route = "/var/backup/ldap/"
 
 days_ago = datetime.now() - timedelta(days=5)
 
 ###logging 
-file = open('/var/backup/scripts/clean.log','a')
+filename = '/var/backup/scripts/clean.log'
+file = open(filename,'a')
 
 
 
 ####
 
+def check(dir=route, idstring="openldap", file=file):
+	check = listdir(dir)
+	for i in check:
+     		if idstring in i:
+         		get = path.getmtime(dir + i)
+         		get1 = datetime.fromtimestamp(get).strftime('%Y-%m-%d')
+         		if get1 <  str(days_ago):
+             			print "Removing files older then 5 days " + i
+             			file.write("Removing files older then 5 days " + i + "\n")
+             			remove(dir + i)
+	file.close()
 
-for i in check:
-     if "openldap" in i:
-         get = path.getmtime(route + i)
-         get1 = datetime.fromtimestamp(get).strftime('%Y-%m-%d')
-         if get1 <  str(days_ago):
-             print "Removing files older then 5 days " + i
-             file.write("Removing files older then 5 days " + i + "\n")
-             remove(route + i)
-file.close()
+if __name__ == '__main__':
+	check()
