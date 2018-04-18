73c41c77fb2372e6a58cc3d971d744e3adff2bd3
Mas <airtime166@yahoo.com>
2018-04-18 13:56:47 -0400

    Create logparse.py

---
+++ python/logparse.py
@@ -0,0 +1,18 @@
+
+# parse log into list and search by date. 
+
+log = open('csa_log.log' , 'r')
+
+
+t = []
+range1 = input("Enter year and month ex: 2018-03: " )
+with log as dipp:
+    data = dipp.read().splitlines()
+
+for view in data:
+    if range1 in view:
+        print(view)
+        t.append(view)
+
+count = len(t)
+print("Total transactions for the month %d" %  (count))
