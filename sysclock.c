1e91c0c714b76dd691919976dd649b272f803cb7
Mas <airtime166@yahoo.com>
2018-05-04 14:24:51 -0400

    Update sysclock.c

--- sysclock.c
+++ sysclock.c
@@ -1,15 +1,23 @@
-/*C program to get current system date and time in Linux.*/
-//https://www.includehelp.com/c-programs/system-date-time-linux.aspx 
 #include <stdio.h>
 #include <time.h>
- 
-int main()
-{
-    time_t T= time(NULL);
-    struct  tm tm = *localtime(&T);
-     
-    printf("System Date is: %02d/%02d/%04d\n",tm.tm_mday, tm.tm_mon+1, tm.tm_year+1900);
-    printf("System Time is: %02d:%02d:%02d\n",tm.tm_hour, tm.tm_min, tm.tm_sec);
- 
-    return 0;
+#include <stdlib.h>
+#include <string.h>
+#include <linux/rtc.h>
+
+
+
+
+int main() {
+
+FILE *ls = popen("/usr/sbin/hwclock -r", "r");
+	char buf[256];
+        while (fgets(buf, sizeof(buf), ls) != 0) {
+        //printf("System Time : %s", buf);
+    for (int i=3; i < 12 && i > 2; i++) {
+        printf("%c", buf[i]);
+    }
+}
+
+pclose(ls);
+
 }
