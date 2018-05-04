6c1807a18740a0646fb34547214650268f48b277
Mas <airtime166@yahoo.com>
2018-05-03 23:13:10 -0400

    Update time.c

--- time.c
+++ time.c
@@ -1,45 +1,42 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Reference http://en.cppreference.com/w/c/chrono/asctime 
-https://www-s.acm.illinois.edu/webmonkeys/book/c_guide/1.2.html#strings
-*/
+ * https://www-s.acm.illinois.edu/webmonkeys/book/c_guide/1.2.html#strings
+ * https://blog.udemy.com/c-string-to-int/  
+ * */
+int main() {
+	FILE *ls = popen("/usr/sbin/hwclock --show | awk '{print $5 }' | egrep -o \"[0-6][0-9]:[0-6][0-9]:\" | sed \'s/://g\' ", "r");
+	char buf[256];
+	while (fgets(buf, sizeof(buf), ls) != 0) {
+    printf("System Time : %s", buf);
 
-int main(){
+}
+pclose(ls);
+    // char os_c = system("date +%I:%M:%S");
 
-        char test[10] = "dipps";
-        time_t result = time(NULL);
+      // check system time 
+      FILE *os_time = popen("date +%I%M", "r");
+        char buf1[256];
+        while (fgets(buf1, sizeof(buf1), os_time) != 0) {
+    printf("OS Time : %s", buf1);
 
-        char os_c = system("hwclock --show");
-        printf("%s\n", test);
-        printf("UTC:%s", asctime(localtime(&result)));  /* print the current OS time. */
-        printf("%s" ,  os_c);
+}
+pclose(os_time);
+int a = atoi(buf);
+int b = atoi(buf1);
 
-
-
-        if (os_c > asctime(localtime(&result)))
-                printf("true");
-        else
-                printf("false");
+if (b < a){
+        int min = a - b ;
+	//printf("The system clock is behind %d %d\n" , a, b);
+        printf("The system clock is behind by %d minutes :\n" , min); 
+}
+else {
+	printf("All good ");
 }
 
 
 
-/* 
-https://github.com/balabit-deps/balabit-os-6-util-linux/blob/18ecbf6e3c975d642b7c7282fe447ce7a8313884/sys-utils/rtcwake.c
-
-
-
-balabit-deps/balabit-os-6-util-linux – rtcwake.c
-Showing the top two matches Last indexed on Mar 6
-84		enum clock_modes clock_mode;	/* hwclock timezone */
-85		time_t sys_time;		/* system time */
-86		time_t rtc_time;		/* hardware time */
-…	
-176		/* Convert rtc_time to normal arithmetic-friendly form,
-177		 * updating tm.tm_wday as used by asctime().
-178		 */
-179		tm.tm_sec = rtc.tm_sec;
-
-*/
+}
