57358f6c9de3b5fef33be2e6460c58ef547eff32
Mas <airtime166@yahoo.com>
2018-05-09 23:05:31 -0400

    Update sysclock.c

--- sysclock.c
+++ sysclock.c
@@ -32,14 +32,18 @@ FILE *ls = popen("/usr/sbin/hwclock -r", "r");
 
 pclose(ls);
 
+//for testing only.
 printf("Testing Sysclock with Char %c%c%c%c\n" , hour[16],hour[17],hour[19],hour[20]);
 
+//converting char to int
 int r = hour[16] - '0';
 int x = hour[17] - '0';
 int y = hour[19] - '0';
 int z = hour[20] - '0';
+
+//putting the int together
 htime= r * 1000 + x * 100 + y * 10 + z;
 
-printf("%i", htime);
+printf("%i\n", htime);
 
 }
