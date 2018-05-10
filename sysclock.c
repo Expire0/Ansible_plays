e0e1391c0af97016cb4453655ec46131bb9a5384
Mas <airtime166@yahoo.com>
2018-05-09 22:19:40 -0400

    Update sysclock.c

--- sysclock.c
+++ sysclock.c
@@ -9,18 +9,19 @@
 
 int main() {
 
-int hour[10];
+int hour[50];
 
 int htime;
 
 int n=0;
 
+int i;
 
 FILE *ls = popen("/usr/sbin/hwclock -r", "r");
 	char buf[256];
         while (fgets(buf, sizeof(buf), ls) != 0) {
         //printf("System Time : %s", buf);
-            for (int i=11; i < 17; i++) {
+            for (i=0; i < 50; i++) {
        // printf("%c", buf[i]);
 
                  hour[n] = buf[i];
@@ -31,11 +32,14 @@ FILE *ls = popen("/usr/sbin/hwclock -r", "r");
 
 pclose(ls);
 
-printf("%c%c%c%c\n" , hour[0],hour[1],hour[3],hour[4]);
+printf("Testing Sysclock with Char %c%c%c%c\n" , hour[16],hour[17],hour[19],hour[20]);
 
+int r = hour[16] - '0';
+int x = hour[17] - '0';
+int y = hour[19] - '0';
+int z = hour[20] - '0';
+htime= r * 1000 + x * 100 + y * 10 + z;
 
-
-
-
+printf("%i", htime);
 
 }
