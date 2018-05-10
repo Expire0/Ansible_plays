4efa2ce990f26571a163446dc6e7762959b58e05
Mas <airtime166@yahoo.com>
2018-05-10 07:28:36 -0400

    Update sysclock.c

--- sysclock.c
+++ sysclock.c
@@ -11,11 +11,11 @@ int main() {
 
 int hour[50], htime , n=0 , i;
 
-FILE *ls = popen("/usr/sbin/hwclock -r", "r");
+FILE *ls = popen("/usr/sbin/hwclock --verbose | grep read", "r");
 	char buf[256];
         while (fgets(buf, sizeof(buf), ls) != 0) {
         //printf("System Time : %s", buf);
-            for (i=0; i < 50; i++) {
+            for (i=0; i < 52; i++) {
        // printf("%c", buf[i]);
 
                  hour[n] = buf[i];
@@ -27,13 +27,13 @@ FILE *ls = popen("/usr/sbin/hwclock -r", "r");
 pclose(ls);
 
 //for testing only.
-printf("Testing Sysclock with Char %c%c%c%c\n" , hour[16],hour[17],hour[19],hour[20]);
+printf("Testing Sysclock with Char %c%c%c%c\n" , hour[42],hour[43],hour[45],hour[46]);
 
 //converting char to int
-int r = hour[16] - '0';
-int x = hour[17] - '0';
-int y = hour[19] - '0';
-int z = hour[20] - '0';
+int r = hour[42] - '0';
+int x = hour[43] - '0';
+int y = hour[45] - '0';
+int z = hour[46] - '0';
 
 //putting the int together
 htime= r * 1000 + x * 100 + y * 10 + z;
