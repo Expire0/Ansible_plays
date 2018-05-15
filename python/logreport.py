195319519c0092e24f4d983415b0c60133b4c5fd
Mas <airtime166@yahoo.com>
2018-05-15 10:45:04 -0400

    Update logreport.py

--- python/logreport.py
+++ python/logreport.py
@@ -1,19 +1,20 @@
-
 # parse log into list and search by date. 
 #library for csa 
 
 log = open('csa_log.log' , 'r')
 
 
-t = []
-range1 = input("Enter year and month ex: 2018-03: " )
-with log as dipp:
-    data = dipp.read().splitlines()
 
-for view in data:
-    if range1 in view:
-        print(view)
-        t.append(view)
+def sort1():
+    t = []
+    range1 = input("Enter year and month ex: 2018-03: " )
+    with log as dipp:
+        data = dipp.read().splitlines()
 
-count = len(t)
-print("Total transactions for the month %d" %  (count))
+    for view in data:
+        if range1 in view:
+            print(view)
+            t.append(view)
+
+    count = len(t)
+    print("Total transactions for the month %d" %  (count))
