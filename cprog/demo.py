cc2c4c0f7b4e7b73fd46d40c4b7aab48f3142363
mas <mas@localhost.localdomain>
2018-07-31 12:49:59 -0400

    clean up files

--- cprog/demo.py
+++ cprog/demo.py
@@ -1,8 +1,11 @@
 import ntplib
 from time import ctime
-import time , calendar
+import time , calendar , os
 import datetime
 import dateutil.relativedelta
+from subprocess import Popen, PIPE
+import sys
+
 
 #reference points 
 #https://stackoverflow.com/questions/12664295/ntp-client-in-python
@@ -14,19 +17,32 @@ hmm =response.tx_time
 #print(hmm[11:-8])
 print(hmm)
 
-hmm2 = calendar.timegm(time.gmtime()) #why use this when you can call time.time() ? 
+
+sub= "@"
+sub1 = sub + str(hmm)
+
+hmm2 = calendar.timegm(time.gmtime()) #why use this when you can call time.time() ? --hel
 
 dt1 = datetime.datetime.fromtimestamp(hmm) # 1973-11-29 2i2:33:09
 dt2 = datetime.datetime.fromtimestamp(hmm2) # 1977-06-07 23:44:50
 rd = dateutil.relativedelta.relativedelta(dt2, dt1)
 
-# 3 years, 6 months, 9 days, 1 hours, 11 minutes and 41 seconds
+# 3 years, 6 months, 9 days, 1 hours, 11 minutes and 41 secondsi
 
 
 mas = rd.minutes
 
 if mas < -1 :
     print(" %d hours, %d minutes and %d seconds" % (rd.hours, rd.minutes, rd.seconds))
-    print("Time is out of sync")
+    print("Time is out of sync & self correctng ")
+    code =Popen(["date", "-s", sub1])   
+    code.communicate()
+
+elif mas > 1 :
+    print(" %d hours, %d minutes and %d seconds" % (rd.hours, rd.minutes, rd.seconds))
+    print("Time is out of sync & self correctng ")
+    code =Popen(["date", "-s", sub1])                               
+    code.communicate()
+
 else:
     print("Time is within the threshold of one min")
